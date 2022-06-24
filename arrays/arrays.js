3d788801fb6bac93e2174ddcd33bd1933e47e64e
Kevanthony <Michael_Curry99@outlook.com>
2022-06-23 21:57:08 -0500

    sol

---
+++ arrays/arrays.js
@@ -0,0 +1,49 @@
+"use strict";
+// question 1
+let chocolateBars = ['snickers','hundred grand','kit kat','skittles']
+
+// question 2
+function addElementToBeginningOfArray (array,element){
+
+    return(array,element);
+}
+
+function destructivelyAddElementToBeginningOfArray (array,element){
+    
+    return(array,element); 
+}
+
+// question 3
+function addElementToEndOfArray (array,element){
+
+    return(array,element);
+}
+
+function destructivelyAddElementToEndOfArray (array,element){
+
+    return(array,element);
+}
+
+// question 4
+function accessElementInArray (){
+
+    return(array,element);
+}
+
+// question 5
+function destructivelyRemoveElementFromBeginningOfArray(){
+
+
+}
+
+// question 6
+function removeElementFromBeginningOfArray(){
+
+
+}
+
+// question 7
+function removeElementFromEndOfArray(){
+
+    
+}
