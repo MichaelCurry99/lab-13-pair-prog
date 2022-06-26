3cba31c74d026bc97091c9d8c306a768e928a390
Kevanthony <Michael_Curry99@outlook.com>
2022-06-26 18:46:56 -0500

    arrays

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -3,47 +3,54 @@
 let chocolateBars = ['snickers','hundred grand','kit kat','skittles']
 
 // question 2
+
+
+
 function addElementToBeginningOfArray (array,element){
+    let country = ["Japan", "China"]
+    country = ["Japan", ...cities]
 
-    return(array,element);
-}
-
-function destructivelyAddElementToBeginningOfArray (array,element){
-    
-    return(array,element); 
+    let numbers = [1, 2, 3]
+numbers[1] = 2
 }
 
 // question 3
 function addElementToEndOfArray (array,element){
-
-    return(array,element);
-}
-
-function destructivelyAddElementToEndOfArray (array,element){
-
-    return(array,element);
+    let myArray = [1, 2, 3]
+    myArray[2] = 3
+     return(myArray[2])
 }
 
 // question 4
 function accessElementInArray (){
 
-    return(array,element);
+    const days = ["Saturday", "Sunday", "Monday"]
+days.shift() 
 }
 
 // question 5
 function destructivelyRemoveElementFromBeginningOfArray(){
+
+    let food = ["Rice", "Salmon", "Cucumber"]
+food.slice(1) // ["Garfield", "Otis"]
 
 
 }
 
 // question 6
 function removeElementFromBeginningOfArray(){
-
+    let bagOfChips = ["Ranch", "Sour Cream", "Barbecue"]
+    bagOfChips.pop() 
+    bagOfChips
+    
 
 }
 
 // question 7
 function removeElementFromEndOfArray(){
 
-    
+
+let bagOfChips = ["Ranch", "Sour Cream", "Barbecue"]
+bagOfChips.slice(0, bagOfChips.length - 1) 
+bagOfChips
 }
